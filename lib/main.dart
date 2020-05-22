8218ce3d4fdc65940ad02f1e5db05a25611fe1d9
KilliannH <killiannhervagault@gmail.com>
2020-05-23 01:10:59 +0200

    need to add some java code for small mediaPlayer functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
-import 'package:drummachineapp/timerService.dart';
+import 'dart:async';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
+
+// run flutter packages get to remove a dependency from pub.yaml will work
+// to fully remove it, go to ./pub-cache/hosted & remove installed package.
+
+// flutter clean will delete build/ & .dart_tool/
 
 class MyApp extends StatelessWidget {
   static const String _title = 'Drum Machine';
@@ -34,12 +40,17 @@ const Bits = [1, 2, 3, 4, 5, 6, 7, 8];
 class _LineStatefulWidgetState extends State<StatefulWidget> {
 
   var _bit = Bits[0];
-  TimerService timer = new TimerService();
 
   updateBit(value) {
     setState(() {
       _bit = value;
     });
+  }
+
+  handleTimer() {
+    const millis = const Duration(milliseconds:500);
+    new Timer.periodic(millis, (Timer t) => print(t.tick.toString())); // tick is an int & it will increment every callbacks
+    // one callback every x millis, setup above.
   }
 
   Widget build(BuildContext context) {
@@ -51,7 +62,7 @@ class _LineStatefulWidgetState extends State<StatefulWidget> {
           onChanged: (value) => updateBit(value),
         );
       }).toList(), RaisedButton(
-        onPressed: () => TimerService.startTimeout(),
+        onPressed: () => handleTimer(),
         child: Text('Start Timer', style: TextStyle(fontSize: 20)),
       ),],
 
